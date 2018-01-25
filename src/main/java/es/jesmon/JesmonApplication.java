7ed3a14517b7f18fc3de2b2f9f6d5da821c0bf24
roberto <roberto@192.168.1.130>
2018-01-25 22:38:49 +0100

    Inicio

---
+++ src/main/java/es/jesmon/JesmonApplication.java
@@ -0,0 +1,12 @@
+package es.jesmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JesmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JesmonApplication.class, args);
+	}
+}
